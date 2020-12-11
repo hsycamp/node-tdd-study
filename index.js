e1b78f07cf879235547d174e302bab76c0204daf
hsycamp <hsycamp@gmail.com>
2020-12-11 14:39:52 +0900

    Feat : 사용자 목록 조회 API 테스트 코드 추가

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const users = [
 app.use(morgan("dev"));
 
 app.get("/users", (req, res) => {
-  res.json(users);
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) return res.status(400).end();
+  res.json(users.slice(0, limit));
 });
 
 app.listen(port, () => {
