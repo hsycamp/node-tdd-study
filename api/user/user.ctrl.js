42c84b468785a5cf70693b83da34a27b696a1f41
hsycamp <hsycamp@gmail.com>
2020-12-14 15:17:22 +0900

    Refactor : db와 index 컨트롤러 연동

--- api/user/user.ctrl.js
+++ api/user/user.ctrl.js
@@ -1,14 +1,13 @@
-let users = [
-  { id: 1, name: "alice" },
-  { id: 2, name: "bek" },
-  { id: 3, name: "chris" },
-];
+const models = require("../../models");
 
 const index = (req, res) => {
   req.query.limit = req.query.limit || 10;
   const limit = parseInt(req.query.limit, 10);
   if (Number.isNaN(limit)) return res.status(400).end();
-  res.json(users.slice(0, limit));
+
+  models.User.findAll({ limit }).then((users) => {
+    res.json(users);
+  });
 };
 
 const show = (req, res) => {
