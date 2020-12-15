9802cde832a83beed53c72de3b6111ccd65abd62
hsycamp <hsycamp@gmail.com>
2020-12-15 15:26:47 +0900

    Refactor : async-await 패턴 적용

--- api/user/user.ctrl.js
+++ api/user/user.ctrl.js
@@ -1,73 +1,65 @@
 const models = require("../../models");
 
-const index = (req, res) => {
+const index = async (req, res) => {
   req.query.limit = req.query.limit || 10;
   const limit = parseInt(req.query.limit, 10);
   if (Number.isNaN(limit)) return res.status(400).end();
 
-  models.User.findAll({ limit }).then((users) => {
-    res.json(users);
-  });
+  const users = await models.User.findAll({ limit });
+  res.json(users);
 };
 
-const show = (req, res) => {
+const show = async (req, res) => {
   const id = parseInt(req.params.id, 10);
   if (Number.isNaN(id)) return res.status(400).end();
 
-  models.User.findOne({ where: { id } }).then((user) => {
+  const user = await models.User.findOne({ where: { id } });
+  if (!user) return res.status(404).end();
+  res.json(user);
+};
+
+const destroy = async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  await models.User.destroy({ where: { id } });
+  res.status(204).end();
+};
+
+const create = async (req, res) => {
+  try {
+    const name = req.body.name;
+    if (!name) return res.status(400).end();
+
+    const user = await models.User.create({ name });
+    res.status(201).json(user);
+  } catch (err) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      return res.status(409).end();
+    }
+    res.status(500).end();
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    if (Number.isNaN(id)) return res.status(400).end();
+
+    const name = req.body.name;
+    if (!name) return res.status(400).end();
+
+    const user = await models.User.findOne({ where: id });
     if (!user) return res.status(404).end();
-    res.json(user);
-  });
-};
-
-const destroy = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (Number.isNaN(id)) return res.status(400).end();
-
-  models.User.destroy({ where: { id } }).then(() => {
-    res.status(204).end();
-  });
-};
-
-const create = (req, res) => {
-  const name = req.body.name;
-  if (!name) return res.status(400).end();
-
-  models.User.create({ name })
-    .then((user) => {
-      res.status(201).json(user);
-    })
-    .catch((err) => {
-      if (err.name === "SequelizeUniqueConstraintError") {
-        return res.status(409).end();
-      }
-      res.status(500).end();
-    });
-};
-
-const update = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (Number.isNaN(id)) return res.status(400).end();
-
-  const name = req.body.name;
-  if (!name) return res.status(400).end();
-
-  models.User.findOne({ where: id }).then((user) => {
-    if (!user) return res.status(404).end();
-
     user.name = name;
-    user
-      .save()
-      .then((_) => {
-        res.json(user);
-      })
-      .catch((err) => {
-        if (err.name === "SequelizeUniqueConstraintError") {
-          return res.status(409).end();
-        }
-        res.status(500).end();
-      });
-  });
+    await user.save();
+    res.json(user);
+  } catch (err) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      return res.status(409).end();
+    }
+    res.status(500).end();
+  }
 };
 
 module.exports = { index, show, destroy, create, update };
--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -9,27 +9,19 @@ describe("GET /users는", () => {
   before(() => models.User.bulkCreate(users));
 
   describe("성공시", () => {
-    it("유저 객체를 담은 배열로 응답한다", (done) => {
-      request(app)
-        .get("/users")
-        .end((err, res) => {
-          res.body.should.be.instanceOf(Array);
-          done();
-        });
+    it("유저 객체를 담은 배열로 응답한다", async () => {
+      const res = await request(app).get("/users");
+      res.body.should.be.instanceOf(Array);
     });
 
-    it("최대 limit 갯수만큼 응답한다", (done) => {
-      request(app)
-        .get("/users?limit=2")
-        .end((err, res) => {
-          res.body.should.have.lengthOf(2);
-          done();
-        });
+    it("최대 limit 갯수만큼 응답한다", async () => {
+      const res = await request(app).get("/users?limit=2");
+      res.body.should.have.lengthOf(2);
     });
   });
   describe("실패시", () => {
-    it("limit이 숫자형이 아니면 400을 응답한다", (done) => {
-      request(app).get("/users?limit=two").expect(400).end(done);
+    it("limit이 숫자형이 아니면 400을 응답한다", async () => {
+      await request(app).get("/users?limit=two").expect(400);
     });
   });
 });
@@ -40,21 +32,17 @@ describe("GET /users/:id는", () => {
   before(() => models.User.bulkCreate(users));
 
   describe("성공시", () => {
-    it("id가 1인 유저 객체를 반환한다.", (done) => {
-      request(app)
-        .get("/users/1")
-        .end((err, res) => {
-          res.body.should.have.property("id", 1);
-          done();
-        });
+    it("id가 1인 유저 객체를 반환한다.", async () => {
+      const res = await request(app).get("/users/1");
+      res.body.should.have.property("id", 1);
     });
   });
   describe("실패시", () => {
-    it("id가 숫자가 아닐 경우 400으로 응답한다.", (done) => {
-      request(app).get("/users/one").expect(400).end(done);
+    it("id가 숫자가 아닐 경우 400으로 응답한다.", async () => {
+      await request(app).get("/users/one").expect(400);
     });
-    it("id로 유저를 찾을 수 없을 경우 400으로 응답한다.", (done) => {
-      request(app).get("/users/999").expect(404).end(done);
+    it("id로 유저를 찾을 수 없을 경우 400으로 응답한다.", async () => {
+      await request(app).get("/users/999").expect(404);
     });
   });
 });
@@ -65,13 +53,13 @@ describe("DELETE /users/:id는", () => {
   before(() => models.User.bulkCreate(users));
 
   describe("성공시", () => {
-    it("204를 응답한다", (done) => {
-      request(app).delete("/users/1").expect(204).end(done);
+    it("204를 응답한다", async () => {
+      await request(app).delete("/users/1").expect(204);
     });
   });
   describe("실패시", () => {
-    it("id가 숫자가 아닐경우 400으로 응답한다", (done) => {
-      request(app).delete("/users/one").expect(400).end(done);
+    it("id가 숫자가 아닐경우 400으로 응답한다", async () => {
+      await request(app).delete("/users/one").expect(400);
     });
   });
 });
@@ -84,15 +72,9 @@ describe("POST /users는", () => {
   describe("성공시", () => {
     let name = "daniel",
       body;
-    before((done) => {
-      request(app)
-        .post("/users")
-        .send({ name })
-        .expect(201)
-        .end((err, res) => {
-          body = res.body;
-          done();
-        });
+    before(async () => {
+      const res = await request(app).post("/users").send({ name }).expect(201);
+      body = res.body;
     });
     it("생성된 유저 객체를 반환한다", () => {
       body.should.have.property("id");
@@ -102,11 +84,11 @@ describe("POST /users는", () => {
     });
   });
   describe("실패시", () => {
-    it("name 파라매터 누락시 400을 반환한다", (done) => {
-      request(app).post("/users").send({}).expect(400).end(done);
+    it("name 파라매터 누락시 400을 반환한다", async () => {
+      await request(app).post("/users").send({}).expect(400);
     });
-    it("name이 중복일 경우 409를 반환한다", (done) => {
-      request(app).post("/users").send({ name: "daniel" }).expect(409).end(done);
+    it("name이 중복일 경우 409를 반환한다", async () => {
+      await request(app).post("/users").send({ name: "daniel" }).expect(409);
     });
   });
 });
@@ -117,29 +99,24 @@ describe("PUT /users/:id는", () => {
   before(() => models.User.bulkCreate(users));
 
   describe("성공시", () => {
-    it("변경된 name을 응답한다", (done) => {
+    it("변경된 name을 응답한다", async () => {
       const name = "chally";
-      request(app)
-        .put("/users/3")
-        .send({ name })
-        .end((err, res) => {
-          res.body.should.have.property("name", name);
-          done();
-        });
+      const res = await request(app).put("/users/3").send({ name });
+      res.body.should.have.property("name", name);
     });
   });
   describe("실패시", () => {
-    it("정수가 아닌 id일 경우 400을 응답한다", (done) => {
-      request(app).put("/users/one").expect(400).end(done);
+    it("정수가 아닌 id일 경우 400을 응답한다", async () => {
+      await request(app).put("/users/one").expect(400);
     });
-    it("name이 없을 경우 400을 응답한다", (done) => {
-      request(app).put("/users/1").send({}).expect(400).end(done);
+    it("name이 없을 경우 400을 응답한다", async () => {
+      await request(app).put("/users/1").send({}).expect(400);
     });
-    it("없는 유저일 경우 404를 응답한다", (done) => {
-      request(app).put("/users/999").send({ name: "foo" }).expect(404).end(done);
+    it("없는 유저일 경우 404를 응답한다", async () => {
+      await request(app).put("/users/999").send({ name: "foo" }).expect(404);
     });
-    it("이름이 중복일 경우 409를 응답한다", (done) => {
-      request(app).put("/users/3").send({ name: "bek" }).expect(409).end(done);
+    it("이름이 중복일 경우 409를 응답한다", async () => {
+      await request(app).put("/users/3").send({ name: "bek" }).expect(409);
     });
   });
 });
