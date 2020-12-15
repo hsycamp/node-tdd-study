2a5c2b42415dc3eb202e21556a3f84b7a9442264
hsycamp <hsycamp@gmail.com>
2020-12-15 18:03:06 +0900

    Refactor : assertion library 변경

--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -1,7 +1,8 @@
 const request = require("supertest");
-const should = require("should");
+const chai = require("chai");
 const app = require("../../");
 const models = require("../../models");
+const expect = chai.expect;
 
 describe("GET /users는", () => {
   const users = [{ name: "alice" }, { name: "bek" }, { name: "chris" }];
@@ -11,17 +12,18 @@ describe("GET /users는", () => {
   describe("성공시", () => {
     it("유저 객체를 담은 배열로 응답한다", async () => {
       const res = await request(app).get("/users");
-      res.body.should.be.instanceOf(Array);
+      expect(res.body).to.be.an("array");
     });
 
     it("최대 limit 갯수만큼 응답한다", async () => {
       const res = await request(app).get("/users?limit=2");
-      res.body.should.have.lengthOf(2);
+      expect(res.body).to.have.lengthOf(2);
     });
   });
   describe("실패시", () => {
     it("limit이 숫자형이 아니면 400을 응답한다", async () => {
-      await request(app).get("/users?limit=two").expect(400);
+      const res = await request(app).get("/users?limit=two");
+      expect(res.status).to.eql(400);
     });
   });
 });
@@ -34,15 +36,17 @@ describe("GET /users/:id는", () => {
   describe("성공시", () => {
     it("id가 1인 유저 객체를 반환한다.", async () => {
       const res = await request(app).get("/users/1");
-      res.body.should.have.property("id", 1);
+      expect(res.body).to.have.property("id", 1);
     });
   });
   describe("실패시", () => {
     it("id가 숫자가 아닐 경우 400으로 응답한다.", async () => {
-      await request(app).get("/users/one").expect(400);
+      const res = await request(app).get("/users/one");
+      expect(res.status).to.eql(400);
     });
     it("id로 유저를 찾을 수 없을 경우 400으로 응답한다.", async () => {
-      await request(app).get("/users/999").expect(404);
+      const res = await request(app).get("/users/999");
+      expect(res.status).to.eql(404);
     });
   });
 });
@@ -54,12 +58,14 @@ describe("DELETE /users/:id는", () => {
 
   describe("성공시", () => {
     it("204를 응답한다", async () => {
-      await request(app).delete("/users/1").expect(204);
+      const res = await request(app).delete("/users/1");
+      expect(res.status).to.eql(204);
     });
   });
   describe("실패시", () => {
     it("id가 숫자가 아닐경우 400으로 응답한다", async () => {
-      await request(app).delete("/users/one").expect(400);
+      const res = await request(app).delete("/users/one");
+      expect(res.status).to.eql(400);
     });
   });
 });
@@ -73,22 +79,25 @@ describe("POST /users는", () => {
     let name = "daniel",
       body;
     before(async () => {
-      const res = await request(app).post("/users").send({ name }).expect(201);
+      const res = await request(app).post("/users").send({ name });
+      expect(res.status).to.eql(201);
       body = res.body;
     });
     it("생성된 유저 객체를 반환한다", () => {
-      body.should.have.property("id");
+      expect(body).to.have.property("id");
     });
     it("입력한 name을 반환한다", () => {
-      body.should.have.property("name", name);
+      expect(body).to.have.property("name", name);
     });
   });
   describe("실패시", () => {
     it("name 파라매터 누락시 400을 반환한다", async () => {
-      await request(app).post("/users").send({}).expect(400);
+      const res = await request(app).post("/users").send({});
+      expect(res.status).to.eql(400);
     });
     it("name이 중복일 경우 409를 반환한다", async () => {
-      await request(app).post("/users").send({ name: "daniel" }).expect(409);
+      const res = await request(app).post("/users").send({ name: "daniel" });
+      expect(res.status).to.eql(409);
     });
   });
 });
@@ -102,21 +111,25 @@ describe("PUT /users/:id는", () => {
     it("변경된 name을 응답한다", async () => {
       const name = "chally";
       const res = await request(app).put("/users/3").send({ name });
-      res.body.should.have.property("name", name);
+      expect(res.body).to.have.property("name", name);
     });
   });
   describe("실패시", () => {
     it("정수가 아닌 id일 경우 400을 응답한다", async () => {
-      await request(app).put("/users/one").expect(400);
+      const res = await request(app).put("/users/one");
+      expect(res.status).to.eql(400);
     });
     it("name이 없을 경우 400을 응답한다", async () => {
-      await request(app).put("/users/1").send({}).expect(400);
+      const res = await request(app).put("/users/1").send({});
+      expect(res.status).to.eql(400);
     });
     it("없는 유저일 경우 404를 응답한다", async () => {
-      await request(app).put("/users/999").send({ name: "foo" }).expect(404);
+      const res = await request(app).put("/users/999").send({ name: "foo" });
+      expect(res.status).to.eql(404);
     });
     it("이름이 중복일 경우 409를 응답한다", async () => {
-      await request(app).put("/users/3").send({ name: "bek" }).expect(409);
+      const res = await request(app).put("/users/3").send({ name: "bek" });
+      expect(res.status).to.eql(409);
     });
   });
 });
